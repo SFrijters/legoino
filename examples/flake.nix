6144e730e65a26eee173463ae7ae5120baef70d4
Stefan Frijters <sfrijters@gmail.com>
2025-04-09 15:47:55 +0200

    Add nix support to examples (tested TrainHub)
    [[ this feature is broken after removal of package-index files ]]
    
    It was a bad idea to include these files and it will be fixed in 1.2.4 .

---
+++ examples/flake.nix
@@ -0,0 +1,121 @@
+{
+  description = "Wrapper for Legoino tests using nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    arduino-nix.url = "github:bouk/arduino-nix";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      arduino-nix,
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system:
+      let
+        inherit (nixpkgs) lib;
+
+        overlays = [
+          arduino-nix.overlay
+          # https://downloads.arduino.cc/packages/package_index.json
+          (arduino-nix.mkArduinoPackageOverlay ./package-index/package_index.json)
+          # https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+          (arduino-nix.mkArduinoPackageOverlay ./package-index/package_esp32_index.json)
+          # https://downloads.arduino.cc/libraries/library_index.json
+          (arduino-nix.mkArduinoLibraryOverlay ./package-index/library_index.json)
+        ];
+
+        pkgs = import nixpkgs {
+          inherit overlays system;
+        };
+
+        arduinoCliShell =
+          let
+            # https://github.com/SFrijters/legoino -> https://github.com/corneliusmunz/legoino
+            Legoino = pkgs.fetchFromGitHub {
+              name = "legoino-sfrijters";
+              owner = "SFrijters";
+              repo = "legoino";
+              tag = "1.2.1";
+              hash = "sha256-+EMk5Lga+gpmXiNKzV1UH9MdloBqS0jWNzsO5HQlDIg=";
+              postFetch = ''
+                mkdir -p $out/libraries/Legoino/
+                mv $out/* $out/libraries/Legoino/ || true
+              '';
+            };
+
+            # https://github.com/h2zero/NimBLE-Arduino
+            NimBLE-Arduino = pkgs.fetchFromGitHub {
+              name = "nimble-arduibo-h2zero-patched";
+              owner = "h2zero";
+              repo = "NimBLE-Arduino";
+              tag = "2.2.3";
+              hash = "sha256-FXAj3E/u1ZvzU1rxeDQ5xZLOIz00bkniJ35YAXTNOG8=";
+              postFetch = ''
+                # Allow more connections
+                substituteInPlace $out/src/nimconfig.h \
+                  --replace-fail "// #define CONFIG_BT_NIMBLE_MAX_CONNECTIONS 3" "#define CONFIG_BT_NIMBLE_MAX_CONNECTIONS 8"
+
+                mkdir -p $out/libraries/NimBLE-Arduino/
+                mv $out/* $out/libraries/NimBLE-Arduino/ || true
+              '';
+            };
+
+            gnumake-wrapper = pkgs.writeShellApplication {
+              name = "make";
+              text = ''
+                ${lib.getExe pkgs.gnumake} _ARDUINO_ROOT_DIR="''${_ARDUINO_ROOT_DIR:-/tmp/arduino}" --file=${./Makefile} "$@"
+              '';
+            };
+
+            arduino-cli-with-packages = pkgs.wrapArduinoCLI {
+              libraries = [
+                Legoino
+                NimBLE-Arduino
+              ];
+
+              packages = [
+                pkgs.arduinoPackages.platforms.esp32.esp32."3.1.3"
+              ];
+            };
+
+            name = "legoino-examples-esp32c3-arduino-cli";
+          in
+            pkgs.mkShell {
+              inherit name;
+
+              packages = with pkgs; [
+                arduino-cli-with-packages
+                gnumake-wrapper
+                picocom # To monitor the serial output
+                python3
+              ];
+
+              # The variables starting with underscores are custom and not used by arduino-cli directly
+              # The _ARDUINO_PROJECT_DIR variable is passed to arduino-cli via the Makefile.
+              arduinoShellHookPaths = ''
+                if [ -z "''${_ARDUINO_PROJECT_DIR:-}" ]; then
+                  if [ -n "''${_ARDUINO_ROOT_DIR:-}" ]; then
+                    export _ARDUINO_PROJECT_DIR="''${_ARDUINO_ROOT_DIR}/${name}"
+                  elif [ -n "''${XDG_CACHE_HOME:-}" ]; then
+                    export _ARDUINO_PROJECT_DIR="''${XDG_CACHE_HOME}/arduino/${name}"
+                  else
+                    export _ARDUINO_PROJECT_DIR="''${HOME}/.arduino/${name}"
+                  fi
+                fi
+              '';
+            };
+      in
+      {
+        devShells = {
+          default = arduinoCliShell;
+        };
+
+        formatter = pkgs.nixfmt-tree;
+      }
+    );
+}
